0f5d047022802ea3fe1fc8b0912a31b385ed65da
ignatown <ignatow.n@mail.ru>
2021-07-09 02:48:03 +0300

    2. Парсинг HTML страницы. [#260358]

---
+++ src/main/java/html/SqlRuParse.java
@@ -0,0 +1,23 @@
+package html;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class SqlRuParse {
+    public static void main(String[] args) throws Exception {
+        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
+        Elements row = doc.select(".postslisttopic");
+        Elements alt = doc.getElementsByClass("altCol");
+        int i = 1;
+        for (Element td : row) {
+            Element href = td.child(0);
+            System.out.println(href.attr("href"));
+            System.out.println(href.text());
+            Element pot = alt.get(i);
+            System.out.println(pot.text());
+            i = i + 2;
+        }
+    }
+}
