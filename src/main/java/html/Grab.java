7dd6661e05dbac52c5faef428de81b9eb4fd2b0b
ignatown <ignatow.n@mail.ru>
2021-07-12 02:57:37 +0300

    3. Архитектура проекта - Аргегатор Java Вакансий [#260359]

---
+++ src/main/java/html/Grab.java
@@ -0,0 +1,8 @@
+package html;
+
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+
+public interface Grab {
+    void init(Parse parse, Store store, Scheduler scheduler) throws SchedulerException;
+}
---
+++ src/main/java/html/Store.java
@@ -0,0 +1,11 @@
+package html;
+
+import java.util.List;
+
+public interface Store {
+    void save(Post post);
+
+    List<Post> getAll();
+
+    Post findById(int id);
+}
