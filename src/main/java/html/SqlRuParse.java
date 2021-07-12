af9bd234394f83cc0f74daca1cb695594343b563
ignatown <ignatow.n@mail.ru>
2021-07-13 01:07:48 +0300

    2.4. SqlRuParse [#285213 #176646]

--- src/main/java/html/SqlRuParse.java
+++ src/main/java/html/SqlRuParse.java
@@ -4,7 +4,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import utils.SqlRuDateTimeParser;
+import utils.DateTimeParser;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -12,14 +12,17 @@ import java.util.List;
 
 public class SqlRuParse implements Parse {
 
-    public static void main(String[] args) throws IOException {
+    private final DateTimeParser dateTimeParser;
 
+    public SqlRuParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
     }
 
+    public static void main(String[] args) {
+    }
 
     @Override
     public List<Post> list(String link) throws IOException {
-        SqlRuDateTimeParser sqlRuDateTimeParser = new SqlRuDateTimeParser();
        List<Post> posts = new ArrayList<>();
        for (int j = 1; j <= 5; j++) {
         Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + j).get();
@@ -30,7 +33,7 @@ public class SqlRuParse implements Parse {
             Element href = td.child(0);
             String parseLink = href.attr("href");
             posts.add(new Post(href.text(), detail(parseLink).getDescription(),
-                    parseLink, sqlRuDateTimeParser.parse(alt.get(i).text())));
+                    parseLink, dateTimeParser.parse(alt.get(i).text())));
             i = i + 2;
         }
     }
@@ -39,10 +42,9 @@ public class SqlRuParse implements Parse {
 
     @Override
     public Post detail(String link) throws IOException {
-        SqlRuDateTimeParser sqlRuDateTimeParser = new SqlRuDateTimeParser();
         Document doc = Jsoup.connect("link").get();
         Elements row = doc.select(".msgBody");
         Elements row1 = doc.select(".msgFooter");
-        return new Post(row.get(1).text(), sqlRuDateTimeParser.parse(row1.get(0).text().split("\\[")[0]));
+        return new Post(row.get(1).text(), dateTimeParser.parse(row1.get(0).text().split("\\[")[0]));
     }
 }
